7ba31d2352edfcf674a1fe8fd0e23d3d632056ad
Bilal Ahsan <live.bilal.ahsan@live.com>
2018-10-22 11:14:09 +0500

    'm_tools-now'

--- SDKSRC/Base/AlgeApp.hpp
+++ SDKSRC/Base/AlgeApp.hpp
@@ -2,7 +2,7 @@
 #include <Box2D/Box2D.h>
 #endif
 #ifndef NO_NATS
-#include "../../Tools/CNetMsg/CNetMsg.h"
+#include "..\..\..\AlgeSDK_m\m_Tools\CNetMsg\CNetMsg.h"
 extern CNetMsg netmsg;
 #endif
 
