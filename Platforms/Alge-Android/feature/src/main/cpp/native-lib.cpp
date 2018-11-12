1ed4695b6ab7803f456262a7b205c4eaca67b68e
AcnodeLabs <three6tdegree@gmail.com>
2018-11-12 21:02:21 +0500

    landscape

--- Platforms/Alge-Android/feature/src/main/cpp/native-lib.cpp
+++ Platforms/Alge-Android/feature/src/main/cpp/native-lib.cpp
@@ -4,9 +4,7 @@
 extern "C" JNIEXPORT jstring
 
 JNICALL
-Java_com_acnodelabs_apps_untitled_feature_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
+Java_com_acnodelabs_apps_untitled_feature_MainActivity_stringFromJNI( JNIEnv *env, jobject /* this */) {
+    std::string hello = "Hello from C++ XAL";
     return env->NewStringUTF(hello.c_str());
 }
