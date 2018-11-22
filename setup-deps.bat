83d98f1842f302feb51d7820c8cabba53a9dfb5e
AcnodeLabs <three6tdegree@gmail.com>
2018-11-22 18:30:07 +0500

    Update setup-deps.bat

--- setup-deps.bat
+++ setup-deps.bat
@@ -1,3 +1,10 @@
-cd ..
-git clone https://github.com/erincatto/Box2D.git
-@echo Pl Add Search Path Settings in XCode/Visual Studio to find Box2D
+Select your Working Directory e.g Desktop
+
+>cd Desktop
+>git clone git clone https://github.com/AcnodeLabs/AlgeSDK.git
+>git clone https://github.com/erincatto/Box2D.git
+>git clone https://github.com/nats-io/cnats.git
+>cd AlgeSDK
+>Kickstart.bat
+
+Add Search Path Settings in XCode/Visual Studio to find Box2D
