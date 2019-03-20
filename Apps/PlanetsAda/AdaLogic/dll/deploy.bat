ac24b999729c0a0af0538fc5f5ba8b510f7e945f
Bilal Ahsan <three6tdegree@gmail.com>
2019-03-20 13:03:01 +0500

    ada code

---
+++ Apps/PlanetsAda/AdaLogic/dll/deploy.bat
@@ -0,0 +1,1 @@
+copy libTe.dll C:\Users\Bilal\Desktop\R1.1412\ALGESDK-BLOB\Platforms\Alge-Win32-VS2012-AdaLib\Debug\
---
+++ Apps/PlanetsAda/PlanetsPlus/PlanetsPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlanetsPlus
+//
+//  Created by alex on 3/26/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
