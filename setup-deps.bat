3afb1cb18c5702d1671370b673b18f8835306a54
AcnodeLabs <three6tdegree@gmail.com>
2019-08-29 11:31:48 +0500

    Update setup-deps.bat

--- setup-deps.bat
+++ setup-deps.bat
@@ -1,5 +1,5 @@
 cd ..
-git clone https://github.com/erincatto/Box2D.git
+git clone https://github.com/erincatto/Box2D.git --branch v2.3.1
 git clone https://github.com/nats-io/cnats.git
 git clone https://github.com/ThemsAllTook/libstem_gamepad.git
 git clone http://github.com/jtsiomb/libdrawtext
