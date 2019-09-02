e649c2117b0c7f85f02779f299d57955837c0aef
AcnodeLabs <three6tdegree@gmail.com>
2019-09-02 23:14:28 +0500

    Add files via upload

---
+++ Platforms/Alge-AndroidVS/src/com/acnodelabs/alge/ALlistener.java
@@ -0,0 +1,6 @@
+package com.acnodelabs.alge;
+
+public interface ALlistener {
+
+	public void SetTitle(String chars);
+}
---
+++ Platforms/Alge-AndroidVS/src/com/acnodelabs/alge/SoundManager.java
@@ -0,0 +1,12 @@
+package com.acnodelabs.alge;
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+public class SoundManager {
+	
+	
+}
