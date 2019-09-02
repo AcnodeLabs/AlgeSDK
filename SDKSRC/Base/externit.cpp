8c994aab9743c0d9da93c67d93c32ec112b77b14
Bilal Ahsan <three6tdegree@gmail.com>
2019-09-02 20:16:11 +0500

    ex

--- SDKSRC/Base/externit.cpp
+++ SDKSRC/Base/externit.cpp
@@ -3,11 +3,88 @@
 
 #ifdef WIN32
 #define APPREF game
+extern "C++" void alAlphaTest(int set_unset, float fA) {
+	if (set_unset) {
+		glAlphaFunc(GL_GREATER, fA);
+		glEnable(GL_ALPHA_TEST);
+		glEnable(GL_DEPTH_TEST);
+	}
+	else {
+		glDisable(GL_ALPHA_TEST);
+	}
+}
+
+extern "C++" void alLoadIdentity() {
+	glLoadIdentity();
+}
+
+extern "C++" void alPushMatrix() {
+	glPushMatrix();
+}
+
+extern "C++" void alPopMatrix() {
+	glPopMatrix();
+}
+
+extern "C++" void alLoadModel(char* alx, char* tga, int id, float size) {
+	string fname = string(alx);
+	if (fname.find(".alx") != string::npos)
+		game.rm.loadAlxModel((char*)alx, (char*)tga, id, size);
+	else //generic 
+		game.rm.loadGenericAsset(fname, id);
+}
+
+int model_counter = -1;
+
+int alLoadModel(ResourceInf* resInf) {
+	alLoadModel((char*)resInf->alx.c_str(), (char*)resInf->tex.c_str(), ++model_counter, resInf->scale);
+	return model_counter;
+}
+
+int alReserveModelId() {
+	++model_counter;
+	return model_counter;
+}
+
+extern "C++" void alDrawModelTranslateRotate(int id, float posx, float posy, float posz,
+	float angle, float x, float y, float z,
+	int rotatefirst,
+	int billboard)
+{
+	glPushMatrix();
+	if (rotatefirst != 0) {
+		glRotatef(angle, x, y, z);
+		glTranslatef(posx, posy, posz);
+	}
+	else {
+		glTranslatef(posx, posy, posz);
+		glRotatef(angle, x, y, z);
+	}
+
+	if (game.rm.models[id]->loaded) {
+		if (billboard == 1) alBillboardBegin();
+		game.rm.models[id]->glDraw();
+		if (billboard == 1) alBillboardEnd();
+	}
+	glPopMatrix();
+}
+
+extern "C++" void alScaleModel(int id, float sx, float sy, float sz) {
+	game.rm.models[id]->SetScale(sx, sy, sz);
+}
+
+extern "C++" void alDrawModel(int id, bool wireframe) {
+	game.rm.models[id]->glDraw(wireframe);
+}
+
+extern "C++" void alTranslateRotate(float posx, float posy, float posz,
+	float angle, float x, float y, float z) {
+	glTranslatef(posx, posy, posz);
+	glRotatef(angle, x, y, z);
+}
+
 #else
 #define APPREF app
-#define EXTERNIT
-#endif
-
 extern class App APPREF;
 
 EXTERNIT void alAlphaTest(int set_unset, float fA) {
@@ -122,5 +199,5 @@ EXTERNIT void alCircle(float r, int segments) {
 
 EXTERNIT void alRect(float w, float h) {
 	APPREF.AL10.Rect(w,h);
-  }
+#endif
 #endif
