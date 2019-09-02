981d836b3c6a75dd269759265840c375e3e31a2c
IMAC <imac@IMACs-iMac.local>
2019-09-02 20:18:56 +0500

    externit mac

--- Platforms/Alge-OSX/alge-mac.cpp
+++ Platforms/Alge-OSX/alge-mac.cpp
@@ -39,7 +39,7 @@ void alPrintText(string text, float scale) {
  if (font!=nullptr) dtx_string(text.c_str());
  glPopMatrix();
 }
-//#include "../../SDKSRC/Base/externit.cpp"
+
 App app;
 
 static int elapsedTime;
@@ -350,73 +350,5 @@ int main( int argc, char** argv )
   return 0;
 }
 
-#define game app
-#ifndef EXTERNIT_CPP_INCLUDED
-extern "C++" void alAlphaTest(int set_unset, float fA) {
-    if (set_unset) {
-        glAlphaFunc(GL_GREATER, fA);
-        glEnable(GL_ALPHA_TEST);
-        glEnable(GL_DEPTH_TEST);
-    } else {
-        glDisable(GL_ALPHA_TEST);
-    }
-}
+#include "../../SDKSRC/Base/externit.cpp"
 
-extern "C++" void alLoadIdentity() {
-    glLoadIdentity();
-}
-
-extern "C++" void alPushMatrix() {
-    glPushMatrix();
-}
-
-extern "C++" void alPopMatrix() {
-    glPopMatrix();
-}
-
-extern "C++" void alLoadModel(char* alx, char* tga, int id, float size) {
-    game.rm.loadAlxModel((char*)alx, (char*)tga, id,size );
-}
-int alLoadModel(ResourceInf* resInf) {
-    static int counter = -1;
-    alLoadModel((char*)resInf->alx.c_str(), (char*)resInf->tex.c_str(), ++counter, resInf->scale);
-    return counter;
-}
-
-extern "C++" void alDrawModelTranslateRotate(int id, float posx , float posy, float posz,
-                                             float angle,float x, float y,float z,
-                                             int rotatefirst ,
-                                             int billboard )
-{
-    glPushMatrix();
-    if (rotatefirst!=0)  {
-        glRotatef(angle, x,y,z);
-        glTranslatef(posx,posy,posz);
-    } else {
-        glTranslatef(posx,posy,posz);
-        glRotatef(angle, x,y,z);
-    }
-    
-    if (game.rm.models[id]->loaded) {
-        if (billboard==1) alBillboardBegin();
-        game.rm.models[id]->glDraw();
-        if (billboard==1) alBillboardEnd();
-    }
-    glPopMatrix();
-}
-
-extern "C++" void alScaleModel(int id, float sx, float sy, float sz) {
-    game.rm.models[id]->SetScale(sx,sy,sz);
-}
-
-extern "C++" void alDrawModel(int id, bool wireframe) {
-    game.rm.models[id]->glDraw(wireframe);
-}
-
-extern "C++" void alTranslateRotate( float posx , float posy, float posz,
-                                    float angle,float x, float y,float z) {
-    glTranslatef(posx,posy,posz);
-    glRotatef(angle,x,y,z);
-}
-
-#endif
--- SDKSRC/Base/externit.cpp
+++ SDKSRC/Base/externit.cpp
@@ -84,120 +84,76 @@ extern "C++" void alTranslateRotate(float posx, float posy, float posz,
 }
 
 #else
-#define APPREF app
-extern class App APPREF;
 
-EXTERNIT void alAlphaTest(int set_unset, float fA) {
-
-  if (set_unset) {
-    glAlphaFunc(GL_GREATER, fA);
-    glEnable(GL_ALPHA_TEST);
-  // glDisable(GL_DEPTH_TEST);
-  //  glEnable(GL_BLEND);
-  //  glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-  //  glBlendEquation(GL_FUNC_ADD);
-  } else {
-    glDisable(GL_ALPHA_TEST);
-  }
-}
-
-
-EXTERNIT void alScalef(float x =1.0, float y=1.0, float z=1.0) {
-  glScalef(x,y,z);
-}
-
-EXTERNIT void alBillBoardBegin() {
-//  billboardBegin();
-
-}
-
-EXTERNIT void alLoadIdentity() {
-  glLoadIdentity();
-}
-
-EXTERNIT void alPushMatrix() {
-  glPushMatrix();
-}
-
-EXTERNIT void alPopMatrix() {
-  glPopMatrix();
-}
-
-EXTERNIT void alaluLookAt(float x1, float y1, float z1, float x2, float y2, float z2, float x, float y, float z) {
-  aluLookAt(x1,y1,z1,x2,y2,z2,x,y,z);
-}
-
-EXTERNIT void alBillBoardEnd() {
- // billboardEnd();
-}
-
-
-EXTERNIT void PushP(int cmd, char* param1, char* param2) {
-	APPREF.output.pushP(cmd, (void*)param1, (void*)param2);
-}
-
-EXTERNIT void PushI(int cmd, int x, int y) {
-	APPREF.output.pushI(cmd,x,y);
-}
-
-EXTERNIT void alTriangle(float size) {
-	APPREF.AL10.Triangle(float(size));
-
-}
-
-EXTERNIT void alLoadModel(char* alx, char* tga, int id, float size) {
-	APPREF.rm.loadAlxModel(alx, tga, id,size );
-}
-
-EXTERNIT void alDrawModelTranslateRotate(int id, float posx , float posy, float posz,
-					   float angle,float x, float y,float z,
- int rotatefirst , int billboard )
-{
-  glPushMatrix();
-  if (rotatefirst!=0)  {
-    glRotatef(angle, x,y,z);
-    glTranslatef(posx,posy,posz);
+//MACOS
+#define game app
+extern "C++" void alAlphaTest(int set_unset, float fA) {
+    if (set_unset) {
+        glAlphaFunc(GL_GREATER, fA);
+        glEnable(GL_ALPHA_TEST);
+        glEnable(GL_DEPTH_TEST);
     } else {
+        glDisable(GL_ALPHA_TEST);
+    }
+}
+
+extern "C++" void alLoadIdentity() {
+    glLoadIdentity();
+}
+
+extern "C++" void alPushMatrix() {
+    glPushMatrix();
+}
+
+extern "C++" void alPopMatrix() {
+    glPopMatrix();
+}
+
+extern "C++" void alLoadModel(char* alx, char* tga, int id, float size) {
+    game.rm.loadAlxModel((char*)alx, (char*)tga, id,size );
+}
+int alLoadModel(ResourceInf* resInf) {
+    static int counter = -1;
+    alLoadModel((char*)resInf->alx.c_str(), (char*)resInf->tex.c_str(), ++counter, resInf->scale);
+    return counter;
+}
+
+extern "C++" void alDrawModelTranslateRotate(int id, float posx , float posy, float posz,
+                                             float angle,float x, float y,float z,
+                                             int rotatefirst ,
+                                             int billboard )
+{
+    glPushMatrix();
+    if (rotatefirst!=0)  {
+        glRotatef(angle, x,y,z);
+        glTranslatef(posx,posy,posz);
+    } else {
+        glTranslatef(posx,posy,posz);
+        glRotatef(angle, x,y,z);
+    }
+    
+    if (game.rm.models[id]->loaded) {
+        if (billboard==1) alBillboardBegin();
+        game.rm.models[id]->glDraw();
+        if (billboard==1) alBillboardEnd();
+    }
+    glPopMatrix();
+}
+
+extern "C++" void alScaleModel(int id, float sx, float sy, float sz) {
+    game.rm.models[id]->SetScale(sx,sy,sz);
+}
+
+extern "C++" void alDrawModel(int id, bool wireframe) {
+    game.rm.models[id]->glDraw(wireframe);
+}
+
+extern "C++" void alTranslateRotate( float posx , float posy, float posz,
+                                    float angle,float x, float y,float z) {
     glTranslatef(posx,posy,posz);
-    glRotatef(angle, x,y,z);
-  }
-
-  if (APPREF.rm.models[id]->loaded) {
-    if (billboard==1) alBillboardBegin();
-	APPREF.rm.models[id]->glDraw();
-    if (billboard==1) alBillboardEnd();
-  }
-  glPopMatrix();
+    glRotatef(angle,x,y,z);
 }
 
-EXTERNIT void alScaleModel(int id, float sx, float sy, float sz) {
-	APPREF.rm.models[id]->SetScale(sx,sy,sz);
-}
-/*
-EXTERNIT void alDrawModel(int id) {
-  APPGAME.rm.models[id]->glDraw();
-}
-*/
-EXTERNIT float alModelBounds(int id) {
- return APPREF.rm.models[id]->boundz();
-}
-
-
-EXTERNIT void alTranslateRotate( float posx , float posy, float posz,
-					             float angle,float x, float y,float z) {
-     glTranslatef(posx,posy,posz);
-     glRotatef(angle,x,y,z);
-}
-
-EXTERNIT void alLine(float x1, float y1, float x2, float y2) {
-	APPREF.AL10.Line(x1,y1,x2,y2);
-  }
-
-EXTERNIT void alCircle(float r, int segments) {
-	APPREF.AL10.Circle(r,short(segments));
-  }
-
-EXTERNIT void alRect(float w, float h) {
-	APPREF.AL10.Rect(w,h);
 #endif
 #endif
+
