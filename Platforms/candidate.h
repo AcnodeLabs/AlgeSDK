c889b7e810668990f9c742f7d0467f4264810c1c
Bilal Ahsan <three6tdegree@gmail.com>
2020-05-09 11:05:44 +0500

    User Story 16: Backup Up and restore All Plat Build OK State (ptime/dtc)

--- Platforms/candidate.h
+++ Platforms/candidate.h
@@ -2,7 +2,7 @@
 // App Folder should Match ProjectName in Solution Explorer
 //
 // In Visual Studio Project Board is accessibale by Tools > Scrumy (Board)
-// or you can goto http://scrummy.com/XAL_PoppingTime
+// or you can goto https://acnodelabs.visualstudio.com/AlgeSDK
 
 #define ALGEAPPNAME "PoppingTime"
 #include "../Apps/PoppingTime/App.hpp"
