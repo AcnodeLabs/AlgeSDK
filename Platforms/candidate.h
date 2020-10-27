622aff24d14c79a74c9e8fa314db50d2e78eccfc
Bilal Ahsan <three6tdegree@gmail.com>
2020-10-27 16:16:22 +0500

    nobox

--- Platforms/candidate.h
+++ Platforms/candidate.h
@@ -3,7 +3,7 @@
 //
 // In Visual Studio Project Board is accessibale by Tools > Scrumy (Board)
 // or you can goto https://acnodelabs.visualstudio.com/AlgeSDK
-#define NO_BOX2D
+//#define NO_BOX2D
 
 #define ALGEAPPNAME "Ivank_Box2D"
 #include "../Apps/Ivank_Box2D/App.hpp"
