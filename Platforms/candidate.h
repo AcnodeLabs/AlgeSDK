c64984e4d1d4079012b390b8feab8daadb60e3d7
Bilal Ahsan <three6tdegree@gmail.com>
2021-02-09 13:08:39 +0500

    ok

--- Platforms/candidate.h
+++ Platforms/candidate.h
@@ -1,3 +1,6 @@
+﻿//				▌║█║▌│║▌│║▌║▌█║Welcome !! ▌│║▌║▌│║║▌█║▌║█
+
+// Tip: Bookmark this file CANDIDATE.h for specifying App to build
 // Available AddOns:-
 // Windows          - CORE+BOX2D+IMGUI+FMOD+NATS
 // macOS            - CORE+BOX2D+IMGUI+FMOD+GNAT
@@ -8,13 +11,13 @@
 // ALGEAPPNAME will be name of Target in Mac, in Windows it is defined by ProjectName  
 #define ALGEAPPNAME "AlgeApp"
 
+// The Canditate to build is referenced below:-
+// For uncluttered workspace 'Do Close All but canditdate.h' and Right Click and Open the file referenced below
+#include "../Apps/XGui/App.hpp"
+
 //For ease of Development Common.Assets/Data is added in the Project Build Phase [mac], so
 //if a new app is added of new asset is added, modify and run collect.sh in app directory once after adding assets, this will also copy the Data of the App to Common.Assets/Data, in Production Use Original App.Assets folder instead of Common.Assets
 
-// Tip: Bookmark CANDIDATE.h for specifying App to build
-// The Canditate to build is referenced below:-
-
-#include "../Apps/XGui/App.hpp"
 
 
 
