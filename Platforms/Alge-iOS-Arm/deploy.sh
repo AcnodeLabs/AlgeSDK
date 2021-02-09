c63fc883d851f38d53423d00ca713c11d436e7b7
Bilal Ahsan <bilal.ahsan@yahoo.com>
2021-02-09 18:20:43 +0500

    deploy

--- Platforms/Alge-iOS-Arm/deploy.sh
+++ Platforms/Alge-iOS-Arm/deploy.sh
@@ -2,12 +2,12 @@ cd ..
 cd ..
 git pull
 cd Platforms
-cd Alge-iPhone
+cd Alge-iOS-Arm
 security unlock-keychain login.keychain
 dir_rel=`xcodebuild -showBuildSettings | grep TARGET_BUILD_DIR | cut -c24-`
 name_app=`xcodebuild -showBuildSettings | grep 'EXECUTABLE_FOLDER_PATH' | cut -c30-`
 dir_dbg=${dir_rel/Release/Debug}
-sch=`xcodebuild -list -project Alge-iOS-XCode10.xcodeproj/ | grep '     '`
+sch=`xcodebuild -list -project Alge-iOS-Arm.xcodeproj/ | grep '     '`
 schemeName="${sch##* }"
 xcodebuild -scheme $schemeName build
 /usr/local/lib/node_modules/ios-deploy/build/Release/ios-deploy --debug --bundle $dir_dbg/$name_app
