3ab5e32067cf1e6a26ff1519f47372121917e1f3
Bilal Ahsan <bilal.ahsan@yahoo.com>
2021-02-09 15:48:11 +0500

    m1 cleanup

--- Platforms/Alge-macOS-Arm/CANDIDATE.h
+++
@@ -1,6 +0,0 @@
-//#define FULLYQUALIFIEDPATH "/Users/Bilal/Desktop/SANDBOX/AlgeSDK/Apps/PoppingTime/App.hpp"
-
-
-//#define CANDIDATE "../../Apps/PoppingTime/App.hpp"
-#include "../candidate.h"
-class App;
