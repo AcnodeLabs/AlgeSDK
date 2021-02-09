0a1361438cd38aa2471acdfd57021ae37fd0be0f
Bilal Ahsan <bilal.ahsan@yahoo.com>
2021-02-09 22:24:44 +0500

    s

--- Platforms/Alge-macOS-Arm/deployex.sh
+++ Platforms/Alge-macOS-Arm/deployex.sh
@@ -1,8 +1,8 @@
 security unlock-keychain login.keychain
 dir_rel=`xcodebuild -showBuildSettings | grep TARGET_BUILD_DIR | cut -c24-`
-name_app=`xcodebuild -showBuildSettings | grep 'EXECUTABLE_FOLDER_PATH' | cut -c30-`
+name_app=`xcodebuild -showBuildSettings | grep 'WRAPPER_NAME' | cut -c20-`
 dir_dbg=${dir_rel/Release/Debug}
-sch=`xcodebuild -list -project Alge-iOS-Arm.xcodeproj/ | grep '     '`
+sch=`xcodebuild -list -project Alge-macOS-Arm.xcodeproj/ | grep '     '`
 schemeName="${sch##* }"
 xcodebuild -scheme $schemeName build
 echo $dir_dbg/$name_app
