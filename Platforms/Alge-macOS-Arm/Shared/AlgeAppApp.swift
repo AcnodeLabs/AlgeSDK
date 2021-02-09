9f4d9aeb6cf651d73fe84625c7896c494f0030fe
Bilal Ahsan <bilal.ahsan@yahoo.com>
2021-02-09 15:53:09 +0500

    ok

---
+++ Platforms/Alge-macOS-Arm/Shared/AlgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgeAppApp.swift
+//  Shared
+//
+//  Created by Xal Mini on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
