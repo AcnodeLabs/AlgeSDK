dcd2340d770b715103558a950cc4256afb7ed58e
Bilal Ahsan <three6tdegree@gmail.com>
2021-03-21 18:20:13 +0500

    a

--- Apps/Planets/Starship.hpp
+++ Apps/Planets/Starship.hpp
@@ -114,45 +114,45 @@ public:
     void Update(GameObject* check, float deltaT) {
         if (check->is(gui) && !gui.hidden)
         {
-            GuiStarts();
-            Show(deltaT);
-            GuiEnds();
+            //GuiStarts();
+            //Show(deltaT);
+            //GuiEnds();
         }
     }
 
     void Show(float dt) {
-        {
-            static string m_vehregno, m_result;
-            static float f = 0.0f;
-            static int counter = 0;
+    //    {
+    //        static string m_vehregno, m_result;
+    //        static float f = 0.0f;
+    //        static int counter = 0;
 
-            ImGui::Begin("Vehicle Lookup");                          // Create a window called "Hello, world!" and append into it.
+    //        ImGui::Begin("Vehicle Lookup");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::InputText("Enter Vehicle Reg No XX-NNN", (char*)m_vehregno.c_str(), 128, 0, 0, 0);
-            bool clicked = ImGui::Button("Go!");
-            if (clicked) {
-                netmsg.Post("veh_regno=" + string(m_vehregno));
-            }
-            ImGui::Text("%s", m_result);
+    //        ImGui::InputText("Enter Vehicle Reg No XX-NNN", (char*)m_vehregno.c_str(), 128, 0, 0, 0);
+    //        bool clicked = ImGui::Button("Go!");
+    //        if (clicked) {
+    //            netmsg.Post("veh_regno=" + string(m_vehregno));
+    //        }
+    //        ImGui::Text("%s", m_result);
 
-            //ImGui::Text("Screen Size = {%d x %d}", msize.x, msize.y);               // Display some text (you can use a format strings too)
-            //ImGui::Text("Mouse Pos = {%d x %d}", mou.x, mou.y);               // Display some text
+    //        //ImGui::Text("Screen Size = {%d x %d}", msize.x, msize.y);               // Display some text (you can use a format strings too)
+    //        //ImGui::Text("Mouse Pos = {%d x %d}", mou.x, mou.y);               // Display some text
 
-            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            //ImGui::Checkbox("Another Window", &show_another_window);
+    //        //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+    //        //ImGui::Checkbox("Another Window", &show_another_window);
 
-            //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+    //        //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+    //        //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            //if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            //    counter++;
-            //ImGui::SameLine();
-            //ImGui::Text("counter = %d", counter);
+    //        //if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+    //        //    counter++;
+    //        //ImGui::SameLine();
+    //        //ImGui::Text("counter = %d", counter);
 
-            //ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::SetWindowPos(ImVec2(0, 50), true);
-            ImGui::End();
-        }
+    //        //ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    //        ImGui::SetWindowPos(ImVec2(0, 50), true);
+    //        ImGui::End();
+    //    }
     }
 };
 
