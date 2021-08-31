685b0654a327e4ea5308b418078aa73b8bad4ce5
acnodelabs <three6tdegreei@gmail.com>
2021-08-31 18:37:39 +0500

    refactor xgui

--- Apps/XGui/App.hpp
+++ Apps/XGui/App.hpp
@@ -4,6 +4,9 @@
    ALGE SDK JD3 Demo :: XGui
 */
 
+//App Specifics
+#include "XGUI_App_Windows.hpp"
+
 class /*XGui*/ App : public AlgeApp {
     
     //Variables
@@ -17,67 +20,12 @@ class /*XGui*/ App : public AlgeApp {
     //Gfx Objects
 	GameObject background, gui;
 
-    //App Specifics
-    class ImWindows {
-        i2 msize;
-        i2 mou;
-        ImFont *roboto;
-    public:
-        void MyFirstToolWindow(float dt) {
-            
-               static float f = 0.0f;
-               static int counter = 0;
-               
-               ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-               ImGui::PushFont(roboto);
-               ImGui::Text("Screen Size = {%d x %d}", msize.x, msize.y);               // Display some text (you can use a format strings too)
-               ImGui::PopFont();
-               ImGui::Text("Mouse Pos = {%d x %d}", mou.x, mou.y);               // Display some text
-            
-               ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-               ImGui::Checkbox("Another Window", &show_another_window);
-
-               ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-               ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-               if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                   counter++;
-               ImGui::SameLine();
-               ImGui::Text("counter = %d", counter);
-
-               ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::SetWindowPos(ImVec2(0, 50), true);
-            ImGui::End();
-        }
-    };
-    void MyFirstToolWindow(float dt) {
     
-        static float f = 0.0f;
-        static int counter = 0;
-        
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-        ImGui::PushFont(roboto);
-        ImGui::Text("Screen Size = {%d x %d}", msize.x, msize.y);               // Display some text (you can use a format strings too)
-        ImGui::PopFont();
-        ImGui::Text("Mouse Pos = {%d x %d}", mou.x, mou.y);               // Display some text
     
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    ImGui::SetWindowPos(ImVec2(0, 50), true);
-    ImGui::End();
-}
-
+    XGUI_App_Windows app_gui;
 public:
+    
+    
     //Primary Functions
     virtual void Init(char* path) {
     
@@ -107,8 +55,7 @@ public:
         if (gob->is(gui) && !gui.hidden)
         {
             GuiStarts();
-                MyFirstToolWindow(deltaT);
-                if (show_demo_window) ImGui::ShowDemoWindow();
+                app_gui.MyFirstToolWindow(deltaT);
             GuiEnds();
         }
 
